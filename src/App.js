7cf3e7c5b2c7379731fb4d5426e7b0bd55748f81
blu3fire <blu3fire89@gmail.com>
2021-02-18 23:25:07 +0800

    Done adding Header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
+import Header from './components/HeaderComponent';
 import Row from './components/RowComponent';
 import request from './components/request';
 import Banner from './components/BannerComponent';
 function App() {
   return (
     <div className="app">
+      <Header />
       <Banner />
-      <center>
-        <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals} isLargeRow/>
-        <Row title="Trending Now" fetchUrl={request.fetchTrending} />
-        <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
-        <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
-        <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
-        <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
-        <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
-        <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
-      </center>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals} isLargeRow/>
+      <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
     </div>
   );
 }
---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from 'react';
+import '../shared/styles/header.css';
+
+function Header() {
+    const[show, handleShow] = useState(false);
+
+    useEffect(() =>{
+        window.addEventListener("scroll", () =>{
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else handleShow(false);
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        }
+    }, []);
+
+    return (
+        <div className={`nav ${show && "nav-black"}`}>
+            <img className="nav-logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
+                alt="netflix logo"
+            />
+            <img className="nav-avatar"
+                src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+                alt="netflix avatar" />
+        </div>
+    )
+}
+
+export default Header
